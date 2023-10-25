35fa63925c1895aa03a2b5605d05d2eb24000edc
thiago-laza <thiago.profmat@gmail.com>
2023-10-25 14:28:20 -0300

    testando commit pelo vscode

--- lista2/ex04.py
+++ lista2/ex04.py
@@ -1,2 +1,4 @@
 """4.Escreva um algoritmo que receba um valor numérico de 3(três) dígitos e exiba a soma dos
 algarismos que compõe este número. Exemplo: 145 -> 1+4+5 = 10"""
+
+n = int(input('Digite um numero de 3 digitos? '))
