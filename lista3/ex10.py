1e3d47aeeefac88ef0352f2193c97161e9a66f2b
thiago-laza <thiago.profmat@gmail.com>
2023-11-04 09:46:29 -0300

    home: testando new

--- lista3/ex10.py
+++ lista3/ex10.py
@@ -1,3 +1,4 @@
 """10. Faça um programa que fique lendo valores até encontrar o valor zero, com cada valor lido faça a
 soma dos 10 valores subsequentes e mostre a soma e a média desses valores."""
 
+print('teste')
