f5ae3729fa01cf30dbc020a23082858d99fe3331
thiago-laza <thiago.profmat@gmail.com>
2023-11-04 16:22:08 -0300

    home: l3 ex10

--- lista3/ex10.py
+++ lista3/ex10.py
@@ -1,4 +1,15 @@
 """10. Faça um programa que fique lendo valores até encontrar o valor zero, com cada valor lido faça a
 soma dos 10 valores subsequentes e mostre a soma e a média desses valores."""
 
-print('teste')
+cont = 0
+acu = 0
+
+while True:
+    num = int(input('Digite um numero: '))
+    if num == 0:
+        break
+    else:
+        acu += num
+        cont += 1
+        if cont == 10:
+            
